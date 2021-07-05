4c95c96f43a1ba2fbe2a2b4050087558d672caa9
ccrcode <ccrcode@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-07-05 07:29:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ccr@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SACCR/R/CalcPFE.R
+++ pkg/SACCR/R/CalcPFE.R
@@ -11,7 +11,9 @@
 #' @references Regulation (EU) 2019/876 of the European Parliament and of the Council of 20 May 2019
 #' http://data.europa.eu/eli/reg/2019/876/oj
 
-CalcPFE <- function(V_C,V,Addon_Aggregate, simplified)  {
+CalcPFE <- function(V_C,Addon_Aggregate, simplified,V=0)  {
+  
+  if(missing(V)) V=V_C
   
   V_C= min(V_C,V)
   if(simplified)
